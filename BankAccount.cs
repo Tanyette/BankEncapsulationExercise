7f2e94422b7a8384c22c1323c2bfc9e107a29fb4
Tanyette Wiggison <bwaffiliates@yahoo.com>
2022-02-18 14:22:12 -0600

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;//private field called balance using camelCase
+
+        public void Deposit(double amount) 
+        {
+            balance += amount;
+        }
+
+        public double GetBalance() 
+        {
+            return balance;
+        }
+
+        public double Withdrawal()
+
+        
+            
+        {
+            Console.WriteLine("what amount do you want to withdrawl?");
+            var isTrue = double.TryParse(Console.ReadLine(), out double amount);
+            return balance -= amount;
+
+          
+        }
+
+        
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAcct = new BankAccount(400.00);
+            Console.WriteLine("New bank account created");
+
+            Console.WriteLine($"Your balance is: {myAcct.GetBalance()}");
+
+            var secondAcct = new BankAccount();
+            Console.WriteLine("2nd account created");
+
+            Console.WriteLine($"Your 2nd account balance is: {secondAcct.GetBalance()}");
+
+            secondAcct.Deposit(200.00);
+            Console.WriteLine("Deposit accepted.");
+            Console.WriteLine($"New balance is:{secondAcct.GetBalance()}");
+        }
+    }
+}
